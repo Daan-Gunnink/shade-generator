76bfdcc613be5fb311e2b88b4287fae46c0beedb
Daan Gunnink <daan@infowijs.nl>
2024-05-13 22:57:43 +0200

    update meta

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,8 +5,11 @@ import GeneralControl from "./GeneralControl";
 import { useEffect, useState } from "react";
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Shade Generator" },
+    {
+      name: "description",
+      content: "Visualize the different shades generated",
+    },
   ];
 };
 
